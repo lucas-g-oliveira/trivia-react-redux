0a4795fa699232405aa8c19b43e58d6ed8bfdc86
Lucas G <lucasg.devmail93@gmail.com>
2022-10-07 18:03:58 -0300

    parte-do-lucas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './trivia.png';
+/* import logo from './trivia.png'; */
 import './App.css';
-// import Login from './pages/Login';
+import Login from './pages/Login';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/*  <header className="App-header">
           <img src={ logo } className="App-logo" alt="logo" />
           <p>SUA VEZ</p>
-        </header>
-        {/* <Login /> */}
+        </header> */}
+        <Login />
       </div>
     );
   }
