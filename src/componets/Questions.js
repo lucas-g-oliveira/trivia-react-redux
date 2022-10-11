41a84ecde806d7dcc95e223b60aafc90c3b2f234
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-10-11 19:11:29 -0300

    [REQ-06] Create game page that should contain information related to the question

--- src/componets/Questions.js
+++ src/componets/Questions.js
@@ -2,34 +2,69 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchQuestions } from '../API/fetchApi';
-import { actionLogin, actionScore, chargeQuestions } from '../Redux/actions';
+import { actionLogin, actionScore } from '../Redux/actions';
 
 class Questions extends React.Component {
   state = {
-    questions: [],
+    question: [],
     currentAnswersRandomized: [],
     loading: true,
+    index: 0,
   };
 
   async componentDidMount() {
     this.loadingData();
   }
 
+  handlerClick = (event) => {
+    const { name } = event.target;
+    if (name === 'CorrectAnswer') {
+      console.log('Acertou!');
+    } else {
+      console.log('Erroooooou!');
+    }
+  };
+
   loadingData = async () => {
     const TOKEN_INVALID = 3;
+    let currentAnswersRandomized = [];
     const { infoUser, clearLogin, history } = this.props;
     const token = localStorage.getItem(infoUser.email);
     const questions = await fetchQuestions(token);
     if (questions.response_code === TOKEN_INVALID) {
-      console.log('ERRO');
       localStorage.clear(infoUser);
       clearLogin({ name: '', email: '' });
       history.push('/');
     }
-    /* chargeStateQuetsions(questions.results); */
-    console.log(questions);
-    this.setState({ questions: questions.results, loading: false });
-    this.butonsOptions();
+    this.setState({
+      question: questions.results, loading: false }, () => {
+      const { question, index } = this.state;
+      const wrongAnswers = question[index].incorrect_answers
+        .map((answers, indexWrong) => (
+          <button
+            type="button"
+            key={ answers }
+            data-testid={ `wrong-answer-${indexWrong}` }
+            name="wrongAnswers"
+            onClick={ this.handlerClick }
+          >
+            {answers}
+          </button>));
+      const answers = [
+        <button
+          type="button"
+          key={ question[index].correct_answer }
+          data-testid="correct-answer"
+          name="CorrectAnswer"
+          onClick={ this.handlerClick }
+        >
+          {question[index].correct_answer}
+        </button>,
+        ...wrongAnswers,
+      ];
+      currentAnswersRandomized = this.randomizeArray(answers);
+      this.setState({ currentAnswersRandomized });
+    });
   };
 
   randomizeArray = (array) => {
@@ -41,106 +76,25 @@ class Questions extends React.Component {
     return arr;
   };
 
-  butonsOptions = () => {
-    const { questions, currentQuestionIndex } = this.state;
-    const currentQuestion = { ...questions[currentQuestionIndex] };
-    if (currentQuestion.type === 'multiple') {
-      const questionsCorrect = {
-        answer: currentQuestion.correct_answer,
-        testId: 'correct-answer',
-        status: 'CorrectAnswer',
-      };
-      const questionsIncorrect = currentQuestion.incorrect_answers
-        .map((e, index) => ({
-          answer: e, testId: `incorrect-answer-${index}`, status: 'IncorrectAnswer',
-        }));
-
-      const newArray = [questionsCorrect, ...questionsIncorrect];
-      this.setState({ currentAnswersRandomized: newArray });
-    }
-  };
-
-  handlerClick = (event) => {
-    const { name } = event.target;
-    /*  const { setScore, score } = this.props; */
-    if (name === 'CorrectAnswer') {
-      console.log('Acertou!');
-      /* setScore({ score: score + 1 }); */
-    } else {
-      console.log('Erroooooou!');
-    }
-  };
-
-  // renderQuestion = () => {
-  //   const { questions, currentAnswersRandomized } = this.state;
-  //   const currentQuestion = { ...questions[0] };
-
-  //   return (
-  //     <div>
-  //       <h4 data-testid="question-category">
-  //         {currentQuestion.category}
-  //       </h4>
-  //       <h4 data-testid="question-text">
-  //         {currentQuestion.question}
-  //       </h4>
-  //       <div datatype="answer-options">
-  //         {currentAnswersRandomized[0]
-  //           ? this.randomizeArray(currentAnswersRandomized).map((e) => (
-  //             <button
-  //               name={ e.status }
-  //               key={ e.answer }
-  //               data-testId={ e.testId }
-  //               type="submit"
-  //               onClick={ this.handlerClick }
-  //             >
-  //               { e.answer }
-  //             </button>
-  //           )) : (<h2> Carregando... </h2>)}
-  //       </div>
-  //     </div>
-  //   );
-  // };
-
   render() {
-    const { loading, questions, currentAnswersRandomized } = this.state;
-    console.log(questions);
+    const { loading, question, currentAnswersRandomized } = this.state;
+    console.log(currentAnswersRandomized);
     return (
-    // <div>
-    //   <div>
-    //     {this.renderQuestion()}
-    //   </div>
-    // </div>
-
       loading ? <p>loading</p>
-        : (<div>
-          test
-          <h4 data-testid="question-category">
-            {questions.category}
-          </h4>
-          <h4 data-testid="question-text">
-            {questions.question}
-          </h4>
-          </ div>),
-          {/* {
-            questions.map(() => (
-              <div datatype="answer-options" key={ Math.random() }>
-                {currentAnswersRandomized[0]
-                  ? this.randomizeArray(currentAnswersRandomized).map((e) => (
-                    <button
-                      name={ e.status }
-                      key={ e.answer }
-                      data-testId={ e.testId }
-                      type="submit"
-                      onClick={ this.handlerClick }
-                    >
-                      { e.answer }
-                    </button>
-                  )) : (<h2> Carregando... </h2>)}
-              </div>
-            ))
-          } */}
-        /* </div> */
-    );
+        : (
+          <div>
+            <h4 data-testid="question-category">
+              {question[0].category}
+            </h4>
+            <h4 data-testid="question-text">
+              {question[0].question}
+            </h4>
+            <div data-testid="answer-options">
+              { currentAnswersRandomized.map((Answer) => Answer) }
+            </div>
+          </div>
+
+        ));
   }
 }
 
@@ -153,14 +107,12 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   clearLogin: (object) => dispatch(actionLogin(object)),
   setScore: (object) => dispatch(actionScore(object)),
-  chargeStateQuetsions: (object) => dispatch(chargeQuestions(object)),
 });
 
 Questions.propTypes = {
   infoUser: PropTypes.objectOf(Object).isRequired,
   clearLogin: PropTypes.func.isRequired,
   // setScore: PropTypes.func.isRequired,
-  /* chargeStateQuetsions: PropTypes.func.isRequired, */
   history: PropTypes.objectOf().isRequired,
   // score: PropTypes.number.isRequired,
 };
